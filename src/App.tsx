2d2616ba4f19baa1c96bbcb6fa52e46235f914c4
nafly09 <juniorinterlyte95@hotmail.com>
2022-06-03 22:05:00 -0300

    feat: finished day 5 code

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { ThemeProvider } from "styled-components";
+import { Provider } from "react-redux";
+import { GlobalStyles } from "./themes/main/global-styles";
 import theme from "./themes/main/theme";
 import Form from "./screens/login/login.screen";
+import store from "./store/store/store";
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Form />;
-    </ThemeProvider>
+    <Provider store={store}>
+      <GlobalStyles />
+      <ThemeProvider theme={theme}>
+        <Form />;
+      </ThemeProvider>
+    </Provider>
   );
 }
 
